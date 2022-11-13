76556ebc030a1e9ff018110377e2ff844d2e8368
Tenkara <rajknakka@gmail.com>
2022-11-13 08:26:12 -0700

    sped up token contract deployment to under 10 secs

--- week3/scripts/DeployMyToken.ts
+++ week3/scripts/DeployMyToken.ts
@@ -1,4 +1,4 @@
-// This script deploys MyToken smart contract based on ERC20 
+// This script deploys a Token smart contract based on ERC20 
 // to a Goerli Testnet
 
 import { ethers } from "ethers";
@@ -8,27 +8,41 @@ import * as dotenv from "dotenv";
 // Read the environment into this script
 dotenv.config();
 
-// Main function for this script
+// Timestamp used with console.log to measure rough performance of this software execution on-chain
+require('log-timestamp');
+
+// Main thinge - thing 1
 async function main() {
     
-    console.log("Deploying MyToken smart contract...");
+    // Start timer to measure elapsed time for the dry run
+        const start = Date.now();
     
-    // set up a Provider
-    const provider = ethers.getDefaultProvider("goerli", process.env.ALCHEMY_API_KEY ?? "");
-    const network = await provider.getNetwork();
+    // Set up a Provider and network
+        console.log("Setting up Alchemy provider with Goerli testnet...\n");
+        const provider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY ?? "");
+        const network = await provider.getNetwork();
     
     // connect Wallet to the Provider
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_ENCODE_BC ?? "");
-    const signer = wallet.connect(provider);
-    const balance = await signer.getBalance();
-    console.log(`Connected to the provider ${network.name} with wallet ${signer.address} and a balance of ${balance}\n`);
-    if(balance.eq(0)) throw new Error("Cannot buy tokens with zero balance in the account\n");
+        console.log("Connecting to a wallet...\n");
+        const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_1 ?? "");
+        const signer = wallet.connect(provider);
+        const balance = await signer.getBalance();
+        console.log(`Connected to the provider ${network.name} with wallet ${signer.address} and a balance of ${balance}\n`);
+        if(balance.eq(0)) throw new Error("Cannot buy tokens with zero balance in the account\n");
 
     // Deploy the token contract factory
-    const tokenContractFactory = new MyToken__factory(signer);
-    const tokenContract = await tokenContractFactory.deploy();
-    await tokenContract.deployed();
-    console.log(`MyToken smart contract is deployed at ${tokenContract.address}\n`)
+        console.log("Initiating Token smart contract deployment...\n");
+        const tokenContractFactory = new MyToken__factory(signer);
+        const tokenContract = await tokenContractFactory.deploy();
+        await tokenContract.deployed();
+        console.log("--------------------------------------------------------------\n");
+        console.log(`Token contract deployed at ${tokenContract.address}\n`);
+        console.log("--------------------------------------------------------------\n");
+
+    // Elapsed time for the dry run
+        let end = Date.now();
+        let elapsed = end - start;
+        console.log(`Elapsed time for the dry run: ${elapsed/1000} seconds.\n`)
 }
 
 main().catch((error) => {
