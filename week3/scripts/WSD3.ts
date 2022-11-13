3afbe92d043ebf93ba5d4453f8aa6f88b1fb5c55
Tenkara <rajknakka@gmail.com>
2022-11-13 07:02:22 -0700

    sped up 30x, switched providers, added wallets

--- week3/scripts/WSD3.ts
+++ week3/scripts/WSD3.ts
@@ -17,15 +17,27 @@
     const MINT_VALUE = ethers.utils.parseEther("10");
 
 async function main() {
-    // set up a Provider
-        const provider = ethers.getDefaultProvider("goerli", process.env.ALCHEMY_API_KEY ?? "");
+    // Start timer to measure elapsed time for the dry run
+        const start = Date.now();
+
+    // set up a Provider and Network
+        console.log("--------------------------------------------------------------\n");
+        console.log(`Starting WSD 3 dry run: Solidity bootcamp week 3 homework...\n`);
+        console.log("--------------------------------------------------------------\n");
+        console.log("Setting up Alchemy provider with Goerli testnet provider...\n");    
+        const provider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY ?? "");
         const network = await provider.getNetwork();
 
     // connect Wallet to the Provider
+        console.log("Connecting to a wallet...\n");
         const wallet1 = new ethers.Wallet(process.env.PRIVATE_KEY_1 ?? "");
         const signer1 = wallet1.connect(provider);
         const wallet2 = new ethers.Wallet(process.env.PRIVATE_KEY_2 ?? "");
         const signer2 = wallet2.connect(provider);
+        const wallet3 = new ethers.Wallet(process.env.PRIVATE_KEY_3 ?? "");
+        const signer3 = wallet3.connect(provider);
+        const wallet4 = new ethers.Wallet(process.env.PRIVATE_KEY_4 ?? "");
+        const signer4 = wallet4.connect(provider);
 
         const balance1 = await signer1.getBalance();
         console.log(`Connected to the provider ${network.name} with wallet ${signer1.address} and a balance of ${balance1}\n`);
@@ -35,22 +47,22 @@ async function main() {
         if(balance2.eq(0)) throw new Error("Cannot buy tokens with zero balance in the account\n");
 
     // Deploy the token contract factory
-    
-        console.log("\n--------------------------------------------------------------\n");
-        console.log(`\nInitiating token contract deployment...\n`);
+        console.log(`Initiating token contract deployment...\n`);
         const tokenContractFactory = new MyToken__factory(signer1);
         const tokenContract = await tokenContractFactory.deploy();
         await tokenContract.deployed();
-    
-        console.log("\n--------------------------------------------------------------\n");
-        console.log(`\nToken contract deployed at ${tokenContract.address}\n`);
-        
+        console.log("--------------------------------------------------------------\n");
+        console.log(`Token contract deployed at ${tokenContract.address}\n`);
+        console.log("--------------------------------------------------------------\n");
     // Check the accounts' token balance before the Mint
         let acc1Balance = await tokenContract.balanceOf(signer1.address);
         let acc2Balance = await tokenContract.balanceOf(signer2.address);
+        let acc3Balance = await tokenContract.balanceOf(signer3.address);
+        let acc4Balance = await tokenContract.balanceOf(signer4.address);
         console.log(`Account ${signer1.address} has a token balance of ${acc1Balance.toString()} before Mint\n`)
         console.log(`Account ${signer2.address} has a token balance of ${acc2Balance.toString()} before Mint\n`)
-        
+        console.log(`Account ${signer3.address} has a token balance of ${acc2Balance.toString()} before Mint\n`)
+        console.log(`Account ${signer4.address} has a token balance of ${acc2Balance.toString()} before Mint\n`)
     // Mint tokens for account 1
         let mintTx = await tokenContract.mint(signer1.address, MINT_VALUE);
         await mintTx.wait()
@@ -60,75 +72,105 @@ async function main() {
         mintTx = await tokenContract.mint(signer2.address, MINT_VALUE);
         await mintTx.wait()
         console.log(`Minted ${MINT_VALUE.toString()} tokens to account ${signer2.address}\n`);
+    
+    // Mint tokens for account 3
+        mintTx = await tokenContract.mint(signer3.address, MINT_VALUE);
+        await mintTx.wait()
+        console.log(`Minted ${MINT_VALUE.toString()} tokens to account ${signer3.address}\n`);
         
+    // Mint tokens for account 4
+        mintTx = await tokenContract.mint(signer4.address, MINT_VALUE);
+        await mintTx.wait()
+        console.log(`Minted ${MINT_VALUE.toString()} tokens to account ${signer4.address}\n`);
+
     // Check the token balances after the Mint
         acc1Balance = await tokenContract.balanceOf(signer1.address);
         acc2Balance = await tokenContract.balanceOf(signer2.address);
+        acc3Balance = await tokenContract.balanceOf(signer3.address);
+        acc4Balance = await tokenContract.balanceOf(signer4.address);
         console.log(`Account ${signer1.address} has a token balance of ${acc1Balance.toString()} after Mint\n`)
         console.log(`Account ${signer2.address} has a token balance of ${acc2Balance.toString()} after Mint\n`)
+        console.log(`Account ${signer3.address} has a token balance of ${acc3Balance.toString()} after Mint\n`)
+        console.log(`Account ${signer4.address} has a token balance of ${acc4Balance.toString()} after Mint\n`)
         
     // Check the voting power before self delegating for both accounts
         let acc1VP = await tokenContract.getVotes(signer1.address);
         let acc2VP = await tokenContract.getVotes(signer2.address);
-        console.log(
-            `Account ${signer1.address} has ${acc1VP.toString()} of voting power before self delegating\n`
-        );
-        console.log(
-            `Account ${signer2.address} has ${acc2VP.toString()} of voting power before self delegating\n`
-        );
+        let acc3VP = await tokenContract.getVotes(signer2.address);
+        let acc4VP = await tokenContract.getVotes(signer2.address);
+        console.log(`Account ${signer1.address} has ${acc1VP.toString()} of voting power before self delegating\n`);
+        console.log(`Account ${signer2.address} has ${acc2VP.toString()} of voting power before self delegating\n`);
+        console.log(`Account ${signer3.address} has ${acc3VP.toString()} of voting power before self delegating\n`);
+        console.log(`Account ${signer4.address} has ${acc4VP.toString()} of voting power before self delegating\n`);
         
     // Self delegate for account 1
-        console.log(
-            `Self delegation for Account ${signer1.address} in progress...\n`
-        );
+        console.log(`Self delegation for Account ${signer1.address} in progress...\n`);
         const delegateTx1 = await tokenContract.connect(signer1).delegate(signer1.address);
         await delegateTx1.wait();
         
     // Self delegate for account 2
-        console.log(
-            `Self delegation for Account ${signer2.address} in progress...\n`
-        );
+        console.log(`Self delegation for Account ${signer2.address} in progress...\n`);
         const delegateTx2 = await tokenContract.connect(signer2).delegate(signer2.address);
         await delegateTx2.wait();
-        
+    
+    // Self delegate for account 3
+        console.log(`Self delegation for Account ${signer3.address} in progress...\n`);
+        const delegateTx3 = await tokenContract.connect(signer3).delegate(signer3.address);
+        await delegateTx2.wait();
+
+    // Self delegate for account 4
+        console.log(`Self delegation for Account ${signer2.address} in progress...\n`);
+        const delegateTx4 = await tokenContract.connect(signer2).delegate(signer2.address);
+        await delegateTx4.wait();
+
     // Check the voting power after self delegating for both accounts
         acc1VP = await tokenContract.getVotes(signer1.address);
-        console.log(
-            `Account ${
-                signer1.address
-            } has ${acc1VP.toString()} voting power after self delegating\n`
-        );
+        console.log(`Account ${signer1.address} has ${acc1VP.toString()} voting power after self delegating\n`);
         acc2VP = await tokenContract.getVotes(signer2.address);
-        console.log(
-            `Account ${
-                signer2.address
-            } has ${acc2VP.toString()} voting power after self delegating\n`
-        );
+        console.log(`Account ${signer2.address} has ${acc2VP.toString()} voting power after self delegating\n`);
+        acc3VP = await tokenContract.getVotes(signer3.address);
+        console.log(`Account ${signer3.address} has ${acc3VP.toString()} voting power after self delegating\n`);
+        acc4VP = await tokenContract.getVotes(signer4.address);
+        console.log(`Account ${signer4.address} has ${acc4VP.toString()} voting power after self delegating\n`);
 
     // Check past voting power
         const lastBlock = await provider.getBlock("latest");
-        console.log(`Current block number is ${lastBlock.number}\n`);
+        console.log(`Last block number is ${lastBlock.number}\n`);
         let pastVotes = await tokenContract.getPastVotes(signer1.address, lastBlock.number - 1);
-        console.log(
-            `Account ${signer1.address} had ${pastVotes.toString()} voting power at previous block\n`)
+        console.log(`Account ${signer1.address} had ${pastVotes.toString()} voting power at previous block\n`)
+        if (pastVotes.eq(0)) {
+            setTimeout(() => {console.log("Slowing down for a few seconds for more on-chain blocks to be produced...\n")}, 10000)
+        }
         pastVotes = await tokenContract.getPastVotes(signer2.address, lastBlock.number - 1);
-            console.log(
-            `Account ${signer2.address} had ${pastVotes.toString()} of voting power at previous block\n`)
-        console.log("\n--------------------------------------------------------------\n");
-
+        console.log(`Account ${signer2.address} had ${pastVotes.toString()} of voting power at previous block\n`)
+        if (pastVotes.eq(0)) {
+            setTimeout(() => {console.log("Slowing down for a few seconds for more on-chain blocks to be produced...\n")}, 10000)
+        }
+        pastVotes = await tokenContract.getPastVotes(signer3.address, lastBlock.number - 1);
+        console.log(`Account ${signer3.address} had ${pastVotes.toString()} of voting power at previous block\n`)
+        if (pastVotes.eq(0)) {
+            setTimeout(() => {console.log("Slowing down for a few seconds for more on-chain blocks to be produced...\n")}, 10000)
+        }
+        pastVotes = await tokenContract.getPastVotes(signer4.address, lastBlock.number - 1);
+        console.log(`Account ${signer4.address} had ${pastVotes.toString()} of voting power at previous block\n`)
+        if (pastVotes.eq(0)) {
+            setTimeout(() => {console.log("Slowing down for a few seconds for more on-chain blocks to be produced...\n")}, 10000)
+        }
     // Deploy TokenBallot contract
         console.log(`Preparing to deploy Token Ballot smart contract...\n`);
-        const proposals = ["Ironman", "captainAmerica", "Spiderman", "captainMarvel", "Wanda", "BlackWidow" ];
-        console.log(`With proposals : \n`);
+        const proposals = ["Ironman", "CaptainAmerica", "Spiderman", "CaptainMarvel", "Wanda", "BlackWidow" ];
+        console.log(`With Superhero proposals : \n`);
         proposals.forEach((element, index) => {
-            console.log(`Proposal N. ${index}: ${element}`);
+            console.log(`Proposal ${index}: ${element}`);
         });
         console.log(`\nInitiating token contract deployment...\n`);
         const ballotContractFactory = new TokenizedBallot__factory(signer1);
         const ballotContract = await ballotContractFactory.deploy(convertStringArrayToBytes32(proposals), tokenContract.address, lastBlock.number - 1);
         await ballotContract.deployed();
     
-        console.log(`\nBallot contract deployed at ${ballotContract.address}\n`);
+        console.log("--------------------------------------------------------------\n");
+        console.log(`Ballot contract deployed at ${ballotContract.address}\n`);
+        console.log("--------------------------------------------------------------\n");
 
     // Let the voting begin
         console.log(`Begin voting...\n`);
@@ -142,8 +184,14 @@ async function main() {
     // Print the winning proposal
         const winningProposal = await ballotContract.winningProposal();
         const winnerName = ethers.utils.parseBytes32String(await ballotContract.winnerName());
-        console.log(`Winning proposal: ${winningProposal}, Winner name: ${winnerName}`);
-        console.log("\n--------------------------------------------------------------\n");
+        console.log("--------------------------------------------------------------\n");
+        console.log(`Winning proposal: ${winningProposal}, Winner name: ${winnerName}\n`);
+        console.log("--------------------------------------------------------------\n");
+    
+    // Elapsed time for the dry run
+        let end = Date.now();
+        let elapsed = end - start;
+        console.log(`Elapsed time for the dry run: ${elapsed/1000} seconds.\n`)
 }
 
 // conversion used to process the proposals from String array to Bytes32 array
