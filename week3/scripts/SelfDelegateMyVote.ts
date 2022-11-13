d0181220a1f94e1973c0ff2488f4117c18963bd1
Tenkara <rajknakka@gmail.com>
2022-11-13 14:28:16 -0700

    sped up delegation to all accounts under mnemonic

--- week3/scripts/SelfDelegateMyVote.ts
+++ week3/scripts/SelfDelegateMyVote.ts
@@ -1,73 +1,77 @@
-// This script self delegates voting power for a given account 
+// This script self delegates voting power for a given set of accounts all at once, like an auto self delegate 
 // usage: yarn run ts-node --files <*SelfDelegateMyVote.ts> <token contract address> <self delegate account address> <address key #>
 
-import { ethers, Wallet } from "ethers";
+import { ethers, Signer, Wallet } from "ethers";
 import { MyToken__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
 
 // Read the environment into this script
 dotenv.config();
 
+// Timestamp used to measure rough performance of this software execution on-chain
+require('log-timestamp');
+
 // Main function for this script
 // Usage: yarn run ts-node --files <dir>/SelfDelegateMyVote.ts <contractAddress> <selfDelegateAddress> <addressLookup>
 async function main() {
-    // Parse the proposals from command line arguments
-    const contractAdress = process.argv[2];
-    const selfDelegateAddress = process.argv[3];
-    const addressLookup = process.argv[4] || "4";
-    console.log(`private key address will be set to key number ${addressLookup}`);
-
-    console.log(`Self delegate voting power of account ${selfDelegateAddress} for smart contract at ${contractAdress} using address key ${addressLookup}...\n`);
     
-    // set up a Provider
-    const provider = ethers.getDefaultProvider("goerli", process.env.ALCHEMY_API_KEY ?? "");
-    const network = await provider.getNetwork();
-    let wallet: Wallet;
-    // connect Wallet to the Provider
-    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_ENCODE_BC ?? "");
-    switch (addressLookup) {
-        case "1": wallet = new ethers.Wallet(process.env.PRIVATE_KEY_1 ?? "");
-        case "2": wallet = new ethers.Wallet(process.env.PRIVATE_KEY_2 ?? "");
-        case "3": wallet = new ethers.Wallet(process.env.PRIVATE_KEY_3 ?? "");
-        case "4": wallet = new ethers.Wallet(process.env.PRIVATE_KEY_4 ?? "");
-        default : wallet = new ethers.Wallet(process.env.PRIVATE_KEY_1 ?? "");
-    }
-    const signer = wallet.connect(provider);
-    const balance = await signer.getBalance();
-    console.log(`Connected to the provider ${network.name} with wallet ${signer.address} and a balance of ${balance}\n`);
-    if(balance.eq(0)) throw new Error("Cannot buy tokens with zero balance in the account\n");
+    // Start timer to measure elapsed time for the dry run
+       const start = Date.now();
 
+    // Parse the token smart contract address from command line arguments
+       const contractAdress = process.argv[2];
+        
+    // set up a Provider
+        const provider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY ?? "");
+        const network = await provider.getNetwork();
+       
+    // Preserve the immutability of the Signers; ensure proper initialization while cycling through the accounts
+        const basepathstr = "m/44'/60'/0'/0/"
+        var signer: Signer[] = new Array(4);
+    
+    // Cycle through the accounts and initialize the signers
+        for( let index = 0; index < 4; index++ ) {
+            signer[index] = (ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "", basepathstr+index.toString())).connect(provider);
+            console.log(`Account${index}: ${await (signer[index]).getAddress()}  Balance: ${await (signer[index]).getBalance()} wei`); 
+        }
+     
     // connect to  MyToken smart contract factory
-    const tokenContractFactory = new MyToken__factory(signer);
-    const tokenContract = await tokenContractFactory.attach(contractAdress);
+        const tokenContractFactory = new MyToken__factory(signer[1]);
+        const tokenContract = await tokenContractFactory.attach(contractAdress);
 
     // Check the account balance before self-delegation
-    const balanceBefore = await tokenContract.balanceOf(selfDelegateAddress);
-    console.log(`Account ${selfDelegateAddress} has a balance of ${balanceBefore.toString()} MyTokens before the Self Delegate\n`) 
+        console.log(`----------------------------------\n`);
+        console.log(`Checking Token balances of all the accounts before self-delegation...`);
+        for(let index = 0; index < signer.length; index++){
+            var balance = await tokenContract.balanceOf(signer[index].getAddress());
+            console.log(`Account${index}: ${await signer[index].getAddress()} has ${balance.toString()} Tokens before the Self Delegate`)
+        }
 
-    // Check the voting power before self-delegation
-    const votingPowerBefore = await tokenContract.getVotes(selfDelegateAddress);
-    console.log(
-        `Account ${selfDelegateAddress} has ${votingPowerBefore.toString()} of MyTokens voting power before self delegating\n`
-    );
     // Self delegate
-    const delegateTx = await tokenContract.delegate(selfDelegateAddress);
- 
-   // Check the voting power after self-delegation
-   const votingPowerAfter = await tokenContract.getVotes(selfDelegateAddress);
-   console.log(
-       `Account ${selfDelegateAddress} has ${votingPowerAfter.toString()} of MyTokens voting power after self delegating\n`
-   );
-
-   // Warn the user about the account owner detail with self delegation
-   if ((votingPowerAfter.sub(votingPowerBefore)).eq(0)) {
-    console.log(`No change in the voting power! Double check that the account used to connect a wallet to the provider is the same account to self-delegate\n`);
-   }
+        console.log(`----------------------------------\n`);
+        console.log(`Initiating self-delegation of all the accounts...`);  
+        for(let index = 0; index < signer.length; index++){
+            var delegateTx = await tokenContract.connect(signer[index]).delegate(signer[index].getAddress());
+            var balance = await tokenContract.balanceOf(signer[index].getAddress());
+            console.log(`Account${index}: ${await signer[index].getAddress()} has ${balance.toString()} Tokens after the Self Delegate`)
+        }
         
-   // Check the account balance before the Mint
-   const balanceAfter = await tokenContract.balanceOf(selfDelegateAddress);
-   console.log(`Account ${selfDelegateAddress} has a balance of ${balanceAfter.toString()} MyTokens before the Self Delegate\n`) 
+   // Check the voting power after self-delegation
+        console.log(`----------------------------------\n`);
+        console.log(`Checking voting power of all the accounts after self-delegation...`);     
+        for(let index = 0; index < signer.length; index++){
+            var votingPower = await tokenContract.getVotes((signer[index]).getAddress());
+            console.log(`Account${index}: ${await signer[index].getAddress()} has ${votingPower.toString()} Tokens after the Self Delegate`)
+        }
+
+    // Elapsed time for the dry run
+        console.log(`----------------------------------\n`);
+        let end = Date.now();
+        let elapsed = end - start;
+        console.log(`Elapsed time for the dry run: ${elapsed/1000} seconds.\n`)
 }
+
+//  Log and exit on error
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
