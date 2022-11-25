ac13986d4365fad94f317d7e6ec069b63cd97a30
Tenkara <rajknakka@gmail.com>
2022-11-25 10:54:55 -0700

    initial commit of a working lottery dApp backend

---
+++ week5/lottery-backend/contracts/Lottery.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {LotteryToken} from "./Token.sol";
+
+/// @title A very simple lottery contract
+/// @author Matheus Pagani
+/// @notice You can use this contract for running a very simple lottery
+/// @dev This contract implements a relatively weak randomness source
+/// @custom:teaching This is a contract meant for teaching only
+contract Lottery is Ownable {
+    /// @notice Address of the token used as payment for the bets
+    LotteryToken public paymentToken;
+    /// @notice Amount of tokens given per ETH paid
+    uint256 public purchaseRatio;
+    /// @notice Amount of tokens required for placing a bet that goes for the prize pool
+    uint256 public betPrice;
+    /// @notice Amount of tokens required for placing a bet that goes for the owner pool
+    uint256 public betFee;
+    /// @notice Amount of tokens in the prize pool
+    uint256 public prizePool;
+    /// @notice Amount of tokens in the owner pool
+    uint256 public ownerPool;
+    /// @notice Flag indicating if the lottery is open for bets
+    bool public betsOpen;
+    /// @notice Timestamp of the lottery next closing date
+    uint256 public betsClosingTime;
+    /// @notice Mapping of prize available for withdraw for each account
+    mapping(address => uint256) public prize;
+
+    /// @dev List of bet slots
+    address[] _slots;
+
+    /// @notice Constructor function
+    /// @param tokenName Name of the token used for payment
+    /// @param tokenSymbol Symbol of the token used for payment
+    /// @param _purchaseRatio Amount of tokens given per ETH paid
+    /// @param _betPrice Amount of tokens required for placing a bet that goes for the prize pool
+    /// @param _betFee Amount of tokens required for placing a bet that goes for the owner pool
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 _purchaseRatio,
+        uint256 _betPrice,
+        uint256 _betFee
+    ) {
+        paymentToken = new LotteryToken(tokenName, tokenSymbol);
+        purchaseRatio = _purchaseRatio;
+        betPrice = _betPrice;
+        betFee = _betFee;
+    }
+
+    /// @notice Passes when the lottery is at closed state
+    modifier whenBetsClosed() {
+        require(!betsOpen, "Lottery is open");
+        _;
+    }
+
+    /// @notice Passes when the lottery is at open state and the current block timestamp is lower than the lottery closing date
+    modifier whenBetsOpen() {
+        require(
+            betsOpen && block.timestamp < betsClosingTime,
+            "Lottery is closed"
+        );
+        _;
+    }
+
+    /// @notice Open the lottery for receiving bets
+    function openBets(uint256 closingTime) public onlyOwner whenBetsClosed {
+        require(
+            closingTime > block.timestamp,
+            "Closing time must be in the future"
+        );
+        betsClosingTime = closingTime;
+        betsOpen = true;
+    }
+
+    /// @notice Give tokens based on the amount of ETH sent
+    function purchaseTokens() public payable {
+        paymentToken.mint(msg.sender, msg.value * purchaseRatio);
+    }
+
+    /// @notice Charge the bet price and create a new bet slot with the sender address
+    function bet() public whenBetsOpen {
+        ownerPool += betFee;
+        prizePool += betPrice;
+        _slots.push(msg.sender);
+        paymentToken.transferFrom(msg.sender, address(this), betPrice + betFee);
+    }
+
+    /// @notice Call the bet function `times` times
+    function betMany(uint256 times) public {
+        require(times > 0);
+        while (times > 0) {
+            bet();
+            times--;
+        }
+    }
+
+    /// @notice Close the lottery and calculates the prize, if any
+    /// @dev Anyone can call this function if the owner fails to do so
+    function closeLottery() public {
+        require(block.timestamp >= betsClosingTime, "Too soon to close");
+        require(betsOpen, "Already closed");
+        if (_slots.length > 0) {
+            uint256 winnerIndex = getRandomNumber() % _slots.length;
+            address winner = _slots[winnerIndex];
+            prize[winner] += prizePool;
+            prizePool = 0;
+            delete (_slots);
+        }
+        betsOpen = false;
+    }
+
+    /// @notice Get a random number calculated from the previous block randao
+    /// @dev This only works after The Merge
+    function getRandomNumber() public view returns (uint256 randomNumber) {
+        randomNumber = block.difficulty;
+    }
+
+    /// @notice Withdraw `amount` from that accounts prize pool
+    function prizeWithdraw(uint256 amount) public {
+        require(amount <= prize[msg.sender], "Not enough prize");
+        prize[msg.sender] -= amount;
+        paymentToken.transfer(msg.sender, amount);
+    }
+
+    /// @notice Withdraw `amount` from the owner pool
+    function ownerWithdraw(uint256 amount) public onlyOwner {
+        require(amount <= ownerPool, "Not enough fees collected");
+        ownerPool -= amount;
+        paymentToken.transfer(msg.sender, amount);
+    }
+
+    /// @notice Burn `amount` tokens and give the equivalent ETH back to user
+    function returnTokens(uint256 amount) public {
+        paymentToken.burnFrom(msg.sender, amount);
+        payable(msg.sender).transfer(amount / purchaseRatio);
+    }
+}
---
+++ week5/lottery-backend/scripts/Lottery.ts
@@ -0,0 +1,285 @@
+import { ethers } from "hardhat";
+import * as readline from "readline";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Lottery, LotteryToken } from "../typechain-types";
+
+let contract: Lottery;
+let token: LotteryToken;
+let accounts: SignerWithAddress[];
+
+const BET_PRICE = 1;
+const BET_FEE = 0.2;
+const TOKEN_RATIO = 1;
+
+async function main() {
+  await initContracts();
+  await initAccounts();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  mainMenu(rl);
+}
+
+async function initContracts() {
+  const contractFactory = await ethers.getContractFactory("Lottery");
+  contract = await contractFactory.deploy(
+    "LotteryToken",
+    "LT0",
+    TOKEN_RATIO,
+    ethers.utils.parseEther(BET_PRICE.toFixed(18)),
+    ethers.utils.parseEther(BET_FEE.toFixed(18))
+  );
+  await contract.deployed();
+  const tokenAddress = await contract.paymentToken();
+  const tokenFactory = await ethers.getContractFactory("LotteryToken");
+  token = tokenFactory.attach(tokenAddress);
+}
+
+async function initAccounts() {
+  accounts = await ethers.getSigners();
+}
+
+async function mainMenu(rl: readline.Interface) {
+  menuOptions(rl);
+}
+
+function menuOptions(rl: readline.Interface) {
+  rl.question(
+    "Select operation: \n Options: \n [0]: Exit \n [1]: Check state \n [2]: Open bets \n [3]: Top up account tokens \n [4]: Bet with account \n [5]: Close bets \n [6]: Check player prize \n [7]: Withdraw \n [8]: Burn tokens \n",
+    async (answer: string) => {
+      console.log(`Selected: ${answer}\n`);
+      const option = Number(answer);
+      switch (option) {
+        case 0:
+          rl.close();
+          return;
+        case 1:
+          await checkState();
+          mainMenu(rl);
+          break;
+        case 2:
+          rl.question("Input duration (in seconds)\n", async (duration) => {
+            try {
+              await openBets(duration);
+            } catch (error) {
+              console.log("error\n");
+              console.log({ error });
+            }
+            mainMenu(rl);
+          });
+          break;
+        case 3:
+          rl.question("What account (index) to use?\n", async (index) => {
+            await displayBalance(index);
+            rl.question("Buy how many tokens?\n", async (amount) => {
+              try {
+                await buyTokens(index, amount);
+                await displayBalance(index);
+                await displayTokenBalance(index);
+              } catch (error) {
+                console.log("error\n");
+                console.log({ error });
+              }
+              mainMenu(rl);
+            });
+          });
+          break;
+        case 4:
+          rl.question("What account (index) to use?\n", async (index) => {
+            await displayTokenBalance(index);
+            rl.question("Bet how many times?\n", async (amount) => {
+              try {
+                await bet(index, amount);
+                await displayTokenBalance(index);
+              } catch (error) {
+                console.log("error\n");
+                console.log({ error });
+              }
+              mainMenu(rl);
+            });
+          });
+          break;
+        case 5:
+          try {
+            await closeLottery();
+          } catch (error) {
+            console.log("error\n");
+            console.log({ error });
+          }
+          mainMenu(rl);
+          break;
+        case 6:
+          rl.question("What account (index) to use?\n", async (index) => {
+            const prize = await displayPrize(index);
+            if (Number(prize) > 0) {
+              rl.question(
+                "Do you want to claim your prize? [Y/N]\n",
+                async (answer) => {
+                  if (answer.toLowerCase() === "y") {
+                    try {
+                      await claimPrize(index, prize);
+                    } catch (error) {
+                      console.log("error\n");
+                      console.log({ error });
+                    }
+                  }
+                  mainMenu(rl);
+                }
+              );
+            } else {
+              mainMenu(rl);
+            }
+          });
+          break;
+        case 7:
+          await displayTokenBalance("0");
+          await displayOwnerPool();
+          rl.question("Withdraw how many tokens?\n", async (amount) => {
+            try {
+              await withdrawTokens(amount);
+            } catch (error) {
+              console.log("error\n");
+              console.log({ error });
+            }
+            mainMenu(rl);
+          });
+          break;
+        case 8:
+          rl.question("What account (index) to use?\n", async (index) => {
+            await displayTokenBalance(index);
+            rl.question("Burn how many tokens?\n", async (amount) => {
+              try {
+                await burnTokens(index, amount);
+              } catch (error) {
+                console.log("error\n");
+                console.log({ error });
+              }
+              mainMenu(rl);
+            });
+          });
+          break;
+        default:
+          throw new Error("Invalid option");
+      }
+    }
+  );
+}
+
+async function checkState() {
+  const state = await contract.betsOpen();
+  console.log(`The lottery is ${state ? "open" : "closed"}\n`);
+  if (!state) return;
+  const currentBlock = await ethers.provider.getBlock("latest");
+  const currentBlockDate = new Date(currentBlock.timestamp * 1000);
+  const closingTime = await contract.betsClosingTime();
+  const closingTimeDate = new Date(closingTime.toNumber() * 1000);
+  console.log(
+    `The last block was mined at ${currentBlockDate.toLocaleDateString()} : ${currentBlockDate.toLocaleTimeString()}\n`
+  );
+  console.log(
+    `lottery should close at  ${closingTimeDate.toLocaleDateString()} : ${closingTimeDate.toLocaleTimeString()}\n`
+  );
+}
+3
+async function openBets(duration: string) {
+  const currentBlock = await ethers.provider.getBlock("latest");
+  const tx = await contract.openBets(currentBlock.timestamp + Number(duration));
+  const receipt = await tx.wait();
+  console.log(`Bets opened (${receipt.transactionHash})`);
+}
+
+async function displayBalance(index: string) {
+  const balanceBN = await ethers.provider.getBalance(
+    accounts[Number(index)].address
+  );
+  const balance = ethers.utils.formatEther(balanceBN);
+  console.log(
+    `The account of address ${
+      accounts[Number(index)].address
+    } has ${balance} ETH\n`
+  );
+}
+
+async function buyTokens(index: string, amount: string) {
+  const tx = await contract.connect(accounts[Number(index)]).purchaseTokens({
+    value: ethers.utils.parseEther(amount).div(TOKEN_RATIO),
+  });
+  const receipt = await tx.wait();
+  console.log(`Tokens bought (${receipt.transactionHash})\n`);
+}
+
+async function displayTokenBalance(index: string) {
+  const balanceBN = await token.balanceOf(accounts[Number(index)].address);
+  const balance = ethers.utils.formatEther(balanceBN);
+  console.log(
+    `The account of address ${
+      accounts[Number(index)].address
+    } has ${balance} LT0\n`
+  );
+}
+
+async function bet(index: string, amount: string) {
+  const allowTx = await token
+    .connect(accounts[Number(index)])
+    .approve(contract.address, ethers.constants.MaxUint256);
+  await allowTx.wait();
+  const tx = await contract.connect(accounts[Number(index)]).betMany(amount);
+  const receipt = await tx.wait();
+  console.log(`Bets placed (${receipt.transactionHash})\n`);
+}
+
+async function closeLottery() {
+  const tx = await contract.closeLottery();
+  const receipt = await tx.wait();
+  console.log(`Bets closed (${receipt.transactionHash})\n`);
+}
+
+async function displayPrize(index: string): Promise<string> {
+  const prizeBN = await contract.prize(accounts[Number(index)].address);
+  const prize = ethers.utils.formatEther(prizeBN);
+  console.log(
+    `The account of address ${
+      accounts[Number(index)].address
+    } has earned a prize of ${prize} Tokens\n`
+  );
+  return prize;
+}
+
+async function claimPrize(index: string, amount: string) {
+  const tx = await contract
+    .connect(accounts[Number(index)])
+    .prizeWithdraw(ethers.utils.parseEther(amount));
+  const receipt = await tx.wait();
+  console.log(`Prize claimed (${receipt.transactionHash})\n`);
+}
+
+async function displayOwnerPool() {
+  const balanceBN = await contract.ownerPool();
+  const balance = ethers.utils.formatEther(balanceBN);
+  console.log(`The owner pool has (${balance}) Tokens \n`);
+}
+
+async function withdrawTokens(amount: string) {
+  const tx = await contract.ownerWithdraw(ethers.utils.parseEther(amount));
+  const receipt = await tx.wait();
+  console.log(`Withdraw confirmed (${receipt.transactionHash})\n`);
+}
+
+async function burnTokens(index: string, amount: string) {
+  const allowTx = await token
+    .connect(accounts[Number(index)])
+    .approve(contract.address, ethers.constants.MaxUint256);
+  const receiptAllow = await allowTx.wait();
+  console.log(`Allowance confirmed (${receiptAllow.transactionHash})\n`);
+  const tx = await contract
+    .connect(accounts[Number(index)])
+    .returnTokens(ethers.utils.parseEther(amount));
+  const receipt = await tx.wait();
+  console.log(`Burn confirmed (${receipt.transactionHash})\n`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
