76ca7ce01b7582bc8d0e258a8e185bc8c36817ff
Tenkara <rajknakka@gmail.com>
2022-11-26 08:03:47 -0700

    sign-in with metamask on localhost

--- week5/lottery-frontend/src/app/app.component.ts
+++ week5/lottery-frontend/src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ethers } from 'ethers';
 
+
+declare global {
+  interface Window {
+    ethereum: ethers.providers.ExternalProvider;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lottery-frontend';
+  provider: ethers.providers.Web3Provider | undefined;
+  account: ethers.Wallet | undefined;
+  signer: ethers.Signer | undefined;
+
+  constructor(private http: HttpClient) {
+    console.log('AppComponent constructor');
+  }
+
+  async connectWallet() {
+    console.log('connectWallet');
+    // Connect to a provider
+    this.provider = new ethers.providers.Web3Provider(window.ethereum);
+    if (window.ethereum) {
+      try {
+        // Request account access if needed
+        this.account = await this.provider.send('eth_requestAccounts', []);
+        console.log('account', this.account);
+        this.signer = await this.provider.getSigner();
+        console.log('signer', this.signer);
+      } catch (error) {
+        console.log('User denied account access');
+      }
+    } else {
+      console.log(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
+    }
+  }
 }
--- week5/lottery-frontend/src/app/app.module.ts
+++ week5/lottery-frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
