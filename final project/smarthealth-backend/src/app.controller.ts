ad17cec71b6c2808a6d5a5da8b81abc3d83c8306
Ken Poon <kenpoon94@gmail.com>
2022-12-07 21:00:12 +0800

    moved typo

--- final project/smarthealth-backend/src/app.controller.ts
+++ final project/smarthealth-backend/src/app.controller.ts
@@ -19,19 +19,18 @@ export class AppController {
   // Todo: Get and Post methods for Owner, HCP sign up screens, and any backend services to test with SwaggerHub
 
   // TODO: Back-end script with end-point for Create EHR screen (Ken)
-  // TODO: Back-end script with end-point for Authorize EHR to HCP screen (Ken)
-  // TODO: Back-end script with end-point for HCP patient info screen (Ken)
-
   @Post('create')
   create(@Body() body: ehrDTO): Promise<{ txHash: string }> {
     return this.appService.create(body);
   }
 
+  // TODO: Back-end script with end-point for Authorize EHR to HCP screen (Ken)
   @Post('authorize')
   authorize(@Body() body: authorizeDTO): Promise<{ txHash: string }> {
     return this.appService.authorize(body);
   }
 
+  // TODO: Back-end script with end-point for HCP patient info screen (Ken)
   @Get('view')
   view(): Promise<any> {
     return this.appService.view();
