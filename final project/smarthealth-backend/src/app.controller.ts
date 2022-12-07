7b071226568b486f1f66162a3c6697c80f50691d
Ken Poon <kenpoon94@gmail.com>
2022-12-07 20:57:37 +0800

    endpoint skeleton

--- final project/smarthealth-backend/src/app.controller.ts
+++ final project/smarthealth-backend/src/app.controller.ts
@@ -5,7 +5,7 @@ import { Query } from '@nestjs/common/decorators';
 import { Address } from 'cluster';
 import { ethers } from 'ethers';
 import { stringify } from 'querystring';
-import { AppService } from './app.service';
+import { AppService, ehrDTO, authorizeDTO } from './app.service';
 
 @Controller()
 export class AppController {
@@ -16,6 +16,24 @@ export class AppController {
     return { result: this.appService.getEhrContractAddress() };
   }
 
-// Todo: Get and Post methods for Owner, HCP sign up screens, and any backend services to test with SwaggerHub
+  // Todo: Get and Post methods for Owner, HCP sign up screens, and any backend services to test with SwaggerHub
 
+  // TODO: Back-end script with end-point for Create EHR screen (Ken)
+  // TODO: Back-end script with end-point for Authorize EHR to HCP screen (Ken)
+  // TODO: Back-end script with end-point for HCP patient info screen (Ken)
+
+  @Post('create')
+  create(@Body() body: ehrDTO): Promise<{ txHash: string }> {
+    return this.appService.create(body);
+  }
+
+  @Post('authorize')
+  authorize(@Body() body: authorizeDTO): Promise<{ txHash: string }> {
+    return this.appService.authorize(body);
+  }
+
+  @Get('view')
+  view(): Promise<any> {
+    return this.appService.view();
+  }
 }
--- final project/smarthealth-backend/src/app.service.ts
+++ final project/smarthealth-backend/src/app.service.ts
@@ -4,6 +4,19 @@ import { BigNumber, ethers } from 'ethers';
 
 // import * as dotenv from "dotenv";
 
+export class ehrDTO {
+  name: string;
+  age: number;
+  sex: string;
+  height: number;
+  weight: number;
+  heartRate: number;
+  bloodPressure: string;
+  oxygenSaturation: number;
+  temperature: number;
+}
+
+export class authorizeDTO {}
 
 @Injectable()
 export class AppService {
@@ -14,44 +27,58 @@ export class AppService {
   ehrContract: ethers.Contract;
   ehrContractOwnerAddress: string;
 
-  constructor() {
-
-  }
+  constructor() {}
 
   // End point for owner sign in screen (Raj)
-  getEhrContractAddress() {  // getter
+  getEhrContractAddress() {
+    // getter
     return this.ehrContract.address;
   }
 
   // End point for HCP sign up screen (Raj)
 
-
   // End point to create a new EHR contract with data from the create EHR screen (Ken)
   deployEHRContract(...args: any[]) {
     let ehrContractAddress: string;
     // Each patient owns and manages their own EHR contract to stay in compliance with HIPAA
-    try { } catch (error) { }
+    try {
+    } catch (error) {}
     return { ehrContractAddress };
   }
 
+  // End point to create EHR metadata (Ken)
+  async create(req: ehrDTO) {
+    let txHash: string;
+    try {
+    } catch (error) {}
+    return { txHash };
+  }
+
   // End point to authorize EHR metadata and details to HCP  (Ken)
-  authorizeEHRDatatoHCP(...args: any[]) {
-    let TxHash: string;
-    try { } catch (error) { }
-    return { TxHash };
+  async authorize(req: authorizeDTO) {
+    let txHash: string;
+    try {
+    } catch (error) {}
+    return { txHash };
   }
 
   // End point to view authorized EHR metadata and details  (Ken)
-  async getAuthorizedData(...args: any[]) {
-    let authorizedData: any;
+  async view() {
+    let authorizedData: ehrDTO = {
+      name: 'Joe Smith',
+      age: 25,
+      sex: 'Male',
+      height: 70,
+      weight: 150,
+      heartRate: 75,
+      bloodPressure: '120/80',
+      oxygenSaturation: 98,
+      temperature: 99.5,
+    };
     try {
-
     } catch (error) {
       console.log(error);
     }
-
     return { authorizedData };
   }
-
-  
 }
