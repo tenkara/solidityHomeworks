22c247993ab29f5edc78cc70be14c8d444ae6fba
Tenkara <rajknakka@gmail.com>
2022-12-08 08:37:03 -0700

    BIP HD wallet for accts, roleplay, signin endpoints setup

--- final project/smarthealth-backend/src/app.controller.ts
+++ final project/smarthealth-backend/src/app.controller.ts
@@ -5,16 +5,21 @@ import { Query } from '@nestjs/common/decorators';
 import { Address } from 'cluster';
 import { ethers } from 'ethers';
 import { stringify } from 'querystring';
-import { AppService, ehrDTO, authorizeDTO } from './app.service';
+import { AppService, ehrDTO, authorizeDTO, rolePlayDTO } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('ehr-address')
-  getEhrContractAddress() {
-    return { result: this.appService.getEhrContractAddress() };
-  }
+  // @Get('signed-name/address?')
+  // initializeAccounts(@Param ('address') address: string): Promise<{ result: string }> {
+  //   return this.appService.initializeAccounts(address);
+  // } 
+
+  @Get('signed-name/address?')
+  initializeAccounts(@Query ('address') address: string): Promise<any> {
+    return this.appService.initializeAccounts(address);
+  } 
 
   // Todo: Get and Post methods for Owner, HCP sign up screens, and any backend services to test with SwaggerHub
 
