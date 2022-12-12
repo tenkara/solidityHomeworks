b19d0ffc235337f5ed6e4e2a9a1d906ccf6cbb01
Tenkara <rajknakka@gmail.com>
2022-12-12 09:06:05 -0700

    merged Ken's changes to the contract

---
+++ final project/smarthealth-contract/hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
+};
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+export default config;
