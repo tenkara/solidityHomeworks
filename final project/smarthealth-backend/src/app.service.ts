b3f117529f25ae225886b813a7e157681618201b
Tenkara <rajknakka@gmail.com>
2022-12-14 06:47:02 -0700

    front end and back end updates from Hali and PR with feedback

--- final project/smarthealth-backend/src/app.service.ts
+++ final project/smarthealth-backend/src/app.service.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import { Address } from 'cluster';
-import { BigNumber, ethers, Signer, Wallet } from 'ethers';
+import { ethers, Signer, Wallet } from 'ethers';
 import {
   isBalanceZero,
   convertToBytes32Array,
@@ -16,22 +15,18 @@ import { EHR } from './entities/ehr.entity';
 import { HCP } from './entities/hcp.entity';
 import { AuthorizeHCPDto } from './dto/hcp.dto';
 require('dotenv').config();
-
 export class rolePlayDTO {
   role: string;
   name: string;
   isOwner: boolean;
 }
-
 interface Wallets {
   patient: Wallet;
   hcp: Wallet;
 }
-
 @Injectable()
 export class AppService {
   provider: ethers.providers.AlchemyProvider;
-
   // Electronic Health Record (EHR) contract variables
   ehrContractFactory: ethers.ContractFactory;
   ehrContract: ethers.Contract;
@@ -42,7 +37,6 @@ export class AppService {
   ownerAddress: string;
   hcpAddress: string;
   contractAddress: string;
-
   constructor() {
     // Initialise patient and hcp signers
     this.signers.patient = getPatient();
@@ -51,7 +45,6 @@ export class AppService {
     this.hcpAddress = this.signers.hcp.address;
   }
   // constructor() {}
-
   // End point for owner, hcp, unknown sign in screen (Raj)
   // Uses the BIP 44 standard for HD wallet derivation path to set up the accounts and their roles for the first iteration
   async initializeAccounts(address: string) {
@@ -63,11 +56,9 @@ export class AppService {
       process.env.ALCHEMY_API_KEY ?? '',
     );
     const network = await provider.getNetwork(); // For later iterations
-
     // Preserve the immutability of the Signers; ensure proper initialization while cycling through the accounts
     const basepathstr = "m/44'/60'/0'/0/";
     const signer: Signer[] = new Array(2); // For now we just use 2 accounts, owner and HCP
-
     // Cycle through the accounts and initialize the signers
     for (let index = 0; index < 2; index++) {
       signer[index] = ethers.Wallet.fromMnemonic(
@@ -80,12 +71,10 @@ export class AppService {
         ].getAddress()}  Balance: ${await signer[index].getBalance()} wei`,
       );
     }
-
     // Determine the account and associatee the roles with the accounts
     // This enables role play of contract owner and HCP and to demonstrate the interaction between the two and the EHR contract
     const ownerAddress = await signer[0].getAddress(); // Owner account for the first itereation
     const hcpAddress = await signer[1].getAddress(); // HCP account for the first iteration
-
     if (ownerAddress === address) {
       this.signedInRole = 'owner';
     } else if (hcpAddress === address) {
@@ -93,7 +82,6 @@ export class AppService {
     } else {
       this.signedInRole = 'unknown';
     }
-
     if (this.signedInRole === 'owner') {
       this.signedInName = process.env.OWNER_NAME ?? '';
     } else if (this.signedInRole === 'hcp') {
@@ -101,18 +89,15 @@ export class AppService {
     } else {
       this.signedInName = 'unknown';
     }
-
     return { result: this.signedInRole };
     // return { rolePlayDTO: { role: this.signedInRole, name: this.signedInName } }; // For subsequent iterations
   }
-
   async checkSignersAddress() {
     return {
       patient: await this.signers.patient.address,
       hcp: await this.signers.hcp.address,
     };
   }
-
   async getRole(address: string): Promise<rolePlayDTO> {
     // Return role determined by address
     switch (address) {
@@ -135,6 +120,7 @@ export class AppService {
 
   // End point to create a new EHR contract with data from the create EHR screen (Ken)
   async deployContract(data: string[]) {
+    console.log('Deploying contract', data);
     let contract = null;
     if (await isBalanceZero(this.signers.patient)) {
       throw new Error('Not enough balance to deploy contract');
@@ -144,7 +130,6 @@ export class AppService {
     await contract.deployed();
     return { address: contract.address, hash: contract.txHash };
   }
-
   // End point to create EHR metadata (Ken)
   // Patient create EHR data first time, contract deployment  (Ken)
   async create(req: CreateEHRDto): Promise<{
@@ -164,7 +149,9 @@ export class AppService {
     ];
     return await this.deployContract(data).then(({ address }) => {
       this.contractAddress = address;
-      return { contractAddress: address, data: req };
+      const result = { contractAddress: address, data: req };
+      console.log('Contract deployed', result);
+      return result;
     });
   }
 
@@ -172,7 +159,7 @@ export class AppService {
   async authorize(
     req: AuthorizeHCPDto,
   ): Promise<{ txHash: string; data: HCP }> {
-    console.log(`Authorise using address : ${this.contractAddress}`);
+    console.log('Authorising HCP', req);
     return await smartHealthContract(this.contractAddress)
       .connect(this.signers.patient)
       .authorizeProvider(
@@ -183,10 +170,12 @@ export class AppService {
       )
       .then(
         ({ transactionHash }) => {
-          return {
+          const result = {
             txHash: transactionHash,
             data: req,
           };
+          console.log('HCP authorised', result);
+          return result;
         },
         (error) => {
           console.log('Authorize error :', error);
@@ -195,17 +184,20 @@ export class AppService {
   }
 
   async viewHCPDetails(): Promise<{ result: any }> {
-    console.log(`View HCP Details using address : ${this.contractAddress}`);
+    console.log(`Getting HCP Details`);
     return await smartHealthContract(this.contractAddress)
       .connect(this.signers.hcp)
       .getHCPDetails(this.signers.hcp.address)
       .then(
         (details) => {
-          return {
+          const result = {
             hcpName: toStr(details.hcpName),
             infoToAuth: toStr(details.infoToAuth),
             reason: toStr(details.reason),
           };
+
+          console.log(`Returning HCP Details`, result);
+          return result;
         },
         (error) => {
           console.log('View HCP Details error :', error);
@@ -215,39 +207,44 @@ export class AppService {
 
   // End point to view authorized EHR metadata and details  (Ken)
   async viewPatientSummary(address: string): Promise<{ result: any }> {
-    console.log(`View patient summary using address : ${this.contractAddress}`);
     const signer = this.proxyAccount(address);
     if (signer === this.signers.patient) {
+      console.log(`Getting Patient Summary as Patient`);
       return await smartHealthContract(this.contractAddress)
         .connect(signer)
         .getPatientSummary()
         .then(
           ({ name, age, birthSex, weight, height }) => {
-            return {
+            const result = {
               name: toStr(name),
               age: toStr(age),
               birthSex: toStr(birthSex),
               weight: toStr(weight),
               height: toStr(height),
             };
+            console.log(`Return Patient Summary`, result);
+            return result;
           },
           (error) => {
             console.log('View patient summary :', error);
           },
         );
     } else {
+      console.log(`Getting Patient Summary as HCP`);
       return await smartHealthContract(this.contractAddress)
         .connect(signer)
         .getPatientSummaryHCP(this.signers.hcp.address)
         .then(
           ({ name, age, birthSex, weight, height }) => {
-            return {
+            const result = {
               name: toStr(name),
               age: toStr(age),
               birthSex: toStr(birthSex),
               weight: toStr(weight),
               height: toStr(height),
             };
+            console.log(`Return Patient Summary`, result);
+            return result;
           },
           (error) => {
             console.log('View patient summary as HCP error :', error);
@@ -257,20 +254,22 @@ export class AppService {
   }
 
   async viewPatientVitals(address: string): Promise<{ result: any }> {
-    console.log(`View patient vitals using address : ${this.contractAddress}`);
     const signer = this.proxyAccount(address);
     if (signer == this.signers.patient) {
+      console.log(`Getting Patient Vitals as Patient`);
       return await smartHealthContract(this.contractAddress)
         .connect(signer)
         .getPatientVitals()
         .then(
           ({ heartRate, bloodPressure, oxygenSat, temperature }) => {
-            return {
+            const result = {
               heartRate: toStr(heartRate),
               bloodPressure: toStr(bloodPressure),
               oxygenSat: toStr(oxygenSat),
               temperature: toStr(temperature),
             };
+            console.log(`Return Patient Vital`, result);
+            return result;
           },
 
           (error) => {
@@ -278,17 +277,20 @@ export class AppService {
           },
         );
     } else {
+      console.log(`Getting Patient Vitals as HCP`);
       return await smartHealthContract(this.contractAddress)
         .connect(signer)
         .getPatientVitalsHCP(this.signers.hcp.address)
         .then(
           ({ heartRate, bloodPressure, oxygenSat, temperature }) => {
-            return {
+            const result = {
               heartRate: toStr(heartRate),
               bloodPressure: toStr(bloodPressure),
               oxygenSat: toStr(oxygenSat),
               temperature: toStr(temperature),
             };
+            console.log(`Return Patient Vital`, result);
+            return result;
           },
           (error) => {
             console.log('View patient vitals as HCP error :', error);
@@ -296,7 +298,6 @@ export class AppService {
         );
     }
   }
-
   // Function as an account selector, this SHOULD NOT be used in production. The front end should determine and
   // redirect patient/hcp API to call the right endpoint instead. This will eventually be deprecated
   proxyAccount = (address: string): Wallet => {
--- final project/smarthealth-frontend/src/app/app.component.ts
+++ final project/smarthealth-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ethers } from 'ethers';
 import { FormControl, FormGroup } from '@angular/forms';
+import { observable } from 'rxjs';
 declare global {
   interface Window {
     ethereum: ethers.providers.ExternalProvider;
@@ -16,17 +17,13 @@ declare global {
 export class AppComponent implements OnInit {
   // HTML variables and contents for the landing page
   title = 'SmartHealth';
-
   // Enable role selection and display the appropriate page
   roleSelected?: number = 3; // 0: Owner, 1: HCP, -1: unknown, 3: not selected (used for landing page empty display)
-
   // Owner sign-in page variables
   ownerName?: string;
   greeting?: string;
-
   // Current menu item selected from Owner menu
   ownerMenuSelected?: number = 0; // For menu options
-
   // Owner create EHR page variables
   contractAddress: string = '';
   name?: string;
@@ -38,15 +35,15 @@ export class AppComponent implements OnInit {
   bloodPressureEHR?: string;
   oxygenSaturationEHR?: number;
   temperatureEHR?: number;
-
   // Owner authorize EHR to HCP page variables
   HCPName?: string;
   vitals?: string;
   reason?: string;
-
   // HCP sign-in page variables
   hcpName?: string;
   hcpAddress?: string;
+  hcpAccessVitals: boolean = false;
+  hcpAccess: boolean = false;
 
   // Current menu item selected from HCP menu
   hcpMenuSelected?: number = 0; // For menu options
@@ -56,7 +53,6 @@ export class AppComponent implements OnInit {
   address: string = ''; // Address of the current account signed in through MetaMask
   signedName?: string; // Name of the current account signed in through MetaMask for later iterations
   signedRole?: string; // Role of the current account signed in through MetaMask
-
   //HCP acces to patient info
   patientName?: string;
   dob?: string;
@@ -64,7 +60,6 @@ export class AppComponent implements OnInit {
   bloodPressure?: string;
   oxygenSaturation?: number;
   temperature?: number;
-
   // Owner HCP access patient info page variables
   //Forms
   sub = new FormGroup({
@@ -87,22 +82,17 @@ export class AppComponent implements OnInit {
       reason: new FormControl('PCP Referral'),
     }),
   });
-
   constructor(private http: HttpClient) {
     console.log('AppComponent constructor');
   }
-
   // For later iterations using lifecycle hooks
   ngOnInit(): void {}
-
   // Connect to MetaMask and sign-in with one of the roles
   async connectWallet() {
     try {
       console.log('connectWallet');
-
       // Connect to the provider
       this.provider = new ethers.providers.Web3Provider(window.ethereum);
-
       if (window.ethereum) {
         // Get the account to use for interaction with SmartHealth contract(s)
         let accounts = await this.provider.send('eth_requestAccounts', []);
@@ -112,7 +102,6 @@ export class AppComponent implements OnInit {
         let queryParams = new HttpParams().append('address', this.address);
         console.log(`account: ${accounts[0]}\n`);
         console.log(`account: ${await this.signer.getAddress()}\n`);
-
         this.http
           .get<any>('http://localhost:3000/signed-name/address', {
             params: queryParams,
@@ -132,7 +121,6 @@ export class AppComponent implements OnInit {
               console.log('unknown role');
             }
           });
-
         this.ownerMenuSelected = -1; // Hide the sign-in button
         this.hcpMenuSelected = -1; // Hide the sign-in button
       } else {
@@ -142,15 +130,12 @@ export class AppComponent implements OnInit {
       console.log(error);
     }
   }
-
   // Simple listener to callback on owner create EHR menu item
   onCreateEHR(menuSelected: number) {
     this.ownerMenuSelected = menuSelected;
   }
-
   submitCreate(data: FormGroup) {
     console.log(data);
-
     this.http
       .post<any>('http://localhost:3000/create', {
         name: this.sub.value.data?.name,
@@ -187,15 +172,12 @@ export class AppComponent implements OnInit {
         );
       });
   }
-
   // Simple listener to callback on owner authorize EHR to HCP menu item
   onAuthorizeHCP(menuSelected: number) {
     this.ownerMenuSelected = menuSelected;
   }
-
   submitAuthorize(hcp: FormGroup) {
     console.log(this.sub2);
-
     this.http
       .post<any>('http://localhost:3000/authorize', {
         name: this.sub2.value.hcp?.HCPName,
@@ -209,45 +191,46 @@ export class AppComponent implements OnInit {
         console.log(ans.data.name, ans.data.auth, ans.data.reason);
       });
   }
-
   // Simple listener to callback on owner sign-out menu item
   onOwnerExit(menuSelected: number) {
     this.ownerMenuSelected = menuSelected;
     this.roleSelected = -1;
   }
-
   // Simple listener to callback on HCP Access patient info menu item
   async onAccessPatientInfo(menuSelected: number) {
     this.hcpMenuSelected = menuSelected;
-
     let queryParams = new HttpParams().append('address', this.address);
-
     try {
       // Need the right endpoint for hcp to view patient vitals
       this.http
         .get<any>('http://localhost:3000/view/vitals', {
           params: queryParams,
+          observe: 'response',
         })
         .subscribe((ans) => {
-          this.heartRate = ans.heartRate;
-          this.bloodPressure = ans.bloodPressure;
-          this.oxygenSaturation = ans.oxygenSat;
-          this.temperature = ans.temperature;
+          this.heartRate = ans.body.heartRate;
+          this.bloodPressure = ans.body.bloodPressure;
+          this.oxygenSaturation = ans.body.oxygenSat;
+          this.temperature = ans.body.temperature;
+          if (ans.status === 200) {
+            this.hcpAccess = true;
+          }
         });
     } catch (error) {
       console.log(error);
     }
   }
-
   onHcpExit(menuSelected: number) {
     this.hcpMenuSelected = menuSelected;
     this.roleSelected = -1;
     console.log(`todo ${menuSelected}`);
   }
-
   submitPatientInfo(patientName: string, dob: string) {
     this.roleSelected = 1;
     this.hcpMenuSelected = 1;
     console.log(`patient: ${patientName} , dob: ${dob} `);
+    if (this.hcpAccess) {
+      this.hcpAccessVitals = true;
+    }
   }
 }
